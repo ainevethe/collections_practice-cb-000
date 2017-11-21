3c381f15a5c62af048a3fbc23d01a783df42455d
Crista Cooper <aine3d@gmail.com>
2017-11-21 01:35:30 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -61,8 +61,7 @@ end
 def add_s(array)
 
   array.each_with_index.collect do |word, index|
-    if word[index] != index[2]
-      word << 's'
+    word << 's' if word[index] != index[2]
       word
     end
   end
