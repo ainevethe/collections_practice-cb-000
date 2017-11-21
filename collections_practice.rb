4741136a3d6cda18fce0ad3e67be5f6a1fae8073
Crista Cooper <aine3d@gmail.com>
2017-11-21 01:32:19 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,69 @@
 
+def sort_array_asc(array)
+
+  array.sort
+
+end
+
+def sort_array_desc(array)
+
+  array.sort! {|a, b| b <=> a}
+
+end
+
+def sort_array_char_count(array)
+
+  array.sort! {|a, b| a.length <=> b.length}
+
+end
+
+def swap_elements(array)
+
+  array[1], array[2] = array[2], array[1]
+  array
+
+end
+
+def reverse_array(array)
+
+  array.reverse
+
+end
+
+def kesha_maker(array)
+  myArray = []
+
+  array.each do |string|
+    i = 2
+  while i < string.length
+    (string[i] = "$")
+    break
+  end
+  string
+  end
+end
+
+def find_a(array)
+
+  allAs = []
+  array.collect do |word|
+  allAs << word if word.start_with?("a")
+  end
+  allAs
+end
+
+def sum_array(array)
+
+  array.inject {|a, b| a+b}
+
+end
+
+def add_s(array)
+
+  array.each_with_index.collect do |word, index|
+    while word[index] != 2
+      word << 's'
+      word
+    end
+  end
+end
